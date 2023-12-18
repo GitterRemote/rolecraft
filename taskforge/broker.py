bcbeed5f16bf730898917358c8f7213391fa3d87
Shuaihu <shuaihu.w@gmail.com>
2023-12-18 11:57:04 +0800

    init

---
+++ taskforge/broker.py
@@ -0,0 +1,72 @@
+import abc
+from abc import abstractmethod
+
+
+class Broker(abc.ABC):
+    @abstractmethod
+    def enqueue(
+        self,
+        task_data: bytes,
+        *,
+        vqueue_name: str = "",
+        priority: int = 50,
+        delay_millis: int = 0,
+    ) -> str:
+        pass
+
+    @abstractmethod
+    def receive(self, max_number: int = 1, wait_time_seconds: int = 0) -> list[Task]:
+        """Receives a list of tasks from the Queue, and the queue will put the tasks on the in process status, which will not be received by other workers before the visibility timeout"""
+        pass
+
+    @abstractmethod
+    def ack(self, task_id: str) -> bool:
+        """Marks the task as completed successfully
+
+        Returns: if the task_id exists and the task is marked as completed, returns True. If the task is not working in process or it is failed permanently, the ack operation will fail to update it as completed.
+        """
+        pass
+
+    @abstractmethod
+    def nack(self, task_id: str, error_message: str) -> bool:
+        """Marks the task as failed.
+
+        Returns: if the task_id exists and the task is marked as failed, returns True. If the task is not working in process or it is compeleted successfully, the ack operation will fail to update it as failed.
+        """
+        pass
+
+    @abstractmethod
+    def requeue(self, task_id: str) -> bool:
+        """Requeue the task when give back the prefetched task. This will mark the task with the idle status.
+
+        Returns:
+        If the task is already ended or in retry, then the operation will fail.
+        """
+        pass
+
+    @abstractmethod
+    def retry(
+        self, task_id: str, delay_millis: int = 0, error_message: str = ""
+    ) -> bool:
+        """Requeue retry-able task. This will mark the task with retry status.
+
+        Returns:
+        If the task is already eneded or in idle, then the operation will failed.
+        """
+        pass
+
+    @abstractmethod
+    def __len__(self) -> int:
+        """Returns the number of uncompleted tasks in th queue, including those are in the process"""
+        pass
+
+    # following methods are for task management.
+
+    @abstractmethod
+    def delete(self, task_id: str) -> bool:
+        """"""
+        pass
+
+    @abstractmethod
+    def update(self, task_id: str, **kwargs):
+        pass
