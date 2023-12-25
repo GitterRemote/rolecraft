24b3c3c0d386e806b5c62966d0e40f02295b7932
Shuaihu <shuaihu.w@gmail.com>
2023-12-25 21:05:45 +0800

    add ut

--- tests/config/test_queue_config.py
+++ tests/config/test_queue_config.py
@@ -1,27 +1,5 @@
 import dataclasses
 import pytest
-from unittest import mock
-from rolecraft.config import queue_config as queue_config_mod
-
-
-@pytest.fixture()
-def broker():
-    return mock.MagicMock()
-
-
-@pytest.fixture()
-def encoder():
-    return mock.MagicMock()
-
-
-@pytest.fixture()
-def queue_config(broker, encoder):
-    return queue_config_mod.QueueConfig(broker=broker, encoder=encoder)
-
-
-@pytest.fixture()
-def incomplete_queue_config(broker, encoder):
-    return queue_config_mod.IncompleteQueueConfig(encoder=encoder)
 
 
 def test_to_queue_config(incomplete_queue_config, broker, queue_config):
