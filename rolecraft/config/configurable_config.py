e8519a1fc5362b0f6e3374a1e79a8adced9b16b5
Shuaihu <shuaihu.w@gmail.com>
2023-12-25 19:09:20 +0800

    refactor config package and service module

---
+++ rolecraft/config/configurable_config.py
@@ -0,0 +1,227 @@
+import dataclasses
+import typing
+from typing import Any, TypeVar, Unpack
+
+from rolecraft import encoder as _encoder
+from rolecraft import middleware as _middleware
+from rolecraft.broker import Broker, HeaderBytesRawMessage
+from rolecraft.encoder import Encoder
+
+from . import config_store as _config_store
+from .queue_config import (
+    IncompleteQueueConfig,
+    QueueConfig,
+    QueueConfigKeys,
+)
+
+M_co = TypeVar("M_co", covariant=True)
+
+
+@dataclasses.dataclass
+class ConfigurableBrokerConfig[M_co]:
+    queue_config: QueueConfig[M_co]
+    queue_configs: dict[str, QueueConfig[M_co]] = dataclasses.field(
+        default_factory=dict
+    )
+
+    def add_queue_config(
+        self,
+        queue_name: str,
+        encoder: Encoder[M_co] | None = None,
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> typing.Self:
+        if encoder:
+            config = dataclasses.replace(
+                self.queue_config, encoder=encoder, **kwds
+            )
+        else:
+            config = dataclasses.replace(self.queue_config, **kwds)
+        self.queue_configs[queue_name] = config
+        return self
+
+
+@dataclasses.dataclass(kw_only=True)
+class InjectableConfig[Q: QueueConfig[Any] | IncompleteQueueConfig[Any]]:
+    queue_configs: dict[str, QueueConfig[Any]] = dataclasses.field(
+        default_factory=dict
+    )
+    broker_configs: dict[
+        Broker[Any], ConfigurableBrokerConfig[Any]
+    ] = dataclasses.field(default_factory=dict)
+    queue_config: Q
+
+    def inject(self):
+        """Inject into config store."""
+        config = self
+        queue_configs = dict(config.queue_configs)
+        broker_queue_configs: dict[Broker[Any], QueueConfig[Any]] = {}
+
+        for broker, broker_config in config.broker_configs.items():
+            queue_configs.update(broker_config.queue_configs)
+            broker_queue_configs[broker] = broker_config.queue_config
+
+        _config_store.ConfigStore(
+            queue_config=config.queue_config,
+            queue_configs=queue_configs,
+            broker_queue_configs=broker_queue_configs,
+        ).set_defaut()
+
+
+@dataclasses.dataclass
+class ConfigurableDefaultConfig[M](InjectableConfig[QueueConfig[M]]):
+    queue_config: QueueConfig[M]
+
+    @typing.overload
+    def add_queue_config(
+        self,
+        queue_name: str,
+        broker: Broker[M] | None = None,
+        encoder: Encoder[M] | None = None,
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> typing.Self:
+        ...
+
+    @typing.overload
+    def add_queue_config[O](
+        self,
+        queue_name: str,
+        broker: Broker[O],
+        encoder: Encoder[O],
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> typing.Self:
+        ...
+
+    def add_queue_config(
+        self,
+        queue_name: str,
+        broker: Broker[Any] | None = None,
+        encoder: Encoder[Any] | None = None,
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> typing.Self:
+        config = dataclasses.replace(self.queue_config, **kwds)
+        if broker:
+            config = dataclasses.replace(config, broker=broker)
+        if encoder:
+            config = dataclasses.replace(config, broker=broker)
+        self.queue_configs[queue_name] = config
+        return self
+
+    @typing.overload
+    def add_broker_config[O](
+        self,
+        broker: Broker[O],
+        encoder: Encoder[O],
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> ConfigurableBrokerConfig[O]:
+        ...
+
+    @typing.overload
+    def add_broker_config(
+        self,
+        broker: Broker[M],
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> ConfigurableBrokerConfig[M]:
+        ...
+
+    def add_broker_config[T](
+        self,
+        broker: Broker[T],
+        encoder: Encoder[T] | None = None,
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> ConfigurableBrokerConfig[T] | ConfigurableBrokerConfig[M]:
+        if encoder:
+            config = dataclasses.replace(
+                self.queue_config, broker=broker, encoder=encoder, **kwds
+            )
+            config = ConfigurableBrokerConfig(queue_config=config)
+        elif self._assert(broker):
+            config = dataclasses.replace(
+                self.queue_config, broker=broker, **kwds
+            )
+            config = ConfigurableBrokerConfig(queue_config=config)
+        else:
+            raise TypeError("Unmatched broker type with encoder")
+
+        self.broker_configs[broker] = config
+        return config
+
+    def _assert(self, broker: Broker[Any]) -> typing.TypeGuard[Broker[M]]:
+        # TODO: implement it
+        return True
+
+
+@dataclasses.dataclass
+class ConfigurableConfig(
+    InjectableConfig[IncompleteQueueConfig[HeaderBytesRawMessage]]
+):
+    queue_config = IncompleteQueueConfig(
+        middlewares=_middleware.MiddlewareList([_middleware.Retryable()]),
+        encoder=_encoder.HeaderBytesEncoder(),
+        consumer_wait_time_seconds=10 * 60,
+    )
+
+    def update_default[M](
+        self,
+        broker: Broker[M],
+        encoder: Encoder[M] | None = None,
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> (
+        ConfigurableDefaultConfig[M]
+        | ConfigurableDefaultConfig[HeaderBytesRawMessage]
+    ):
+        """Once update default, you should discard current config instance and use new returned config for later configuration"""
+        if encoder:
+            config = dataclasses.replace(
+                self.queue_config, broker=broker, encoder=encoder, **kwds
+            ).to_queue_config()
+            return ConfigurableDefaultConfig(
+                queue_config=config,
+                queue_configs=self.queue_configs,
+                broker_configs=self.broker_configs,
+            )
+        elif self._assert(broker):
+            config = dataclasses.replace(
+                self.queue_config, broker=broker, **kwds
+            ).to_queue_config()
+            return ConfigurableDefaultConfig(
+                queue_config=config,
+                queue_configs=self.queue_configs,
+                broker_configs=self.broker_configs,
+            )
+        else:
+            raise TypeError("Unmatched broker type with encoder")
+
+    def _assert(
+        self, broker: Broker[Any]
+    ) -> typing.TypeGuard[Broker[HeaderBytesRawMessage]]:
+        # TODO: implement it
+        return True
+
+    def add_broker_config[M](
+        self,
+        broker: Broker[M],
+        encoder: Encoder[M],
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> ConfigurableBrokerConfig[M]:
+        options = dataclasses.asdict(
+            dataclasses.replace(self.queue_config, **kwds)
+        )
+        options.pop("broker")
+        options.pop("encoder")
+        config = QueueConfig(broker=broker, encoder=encoder, **options)
+        broker_config = ConfigurableBrokerConfig(queue_config=config)
+        self.broker_configs[broker] = broker_config
+        return broker_config
+
+    def add_queue_config[M](
+        self,
+        queue_name: str,
+        broker: Broker[M],
+        encoder: Encoder[M],
+        **kwds: Unpack[QueueConfigKeys],
+    ) -> typing.Self:
+        config = dataclasses.replace(
+            self.queue_config, broker=broker, encoder=encoder, **kwds
+        ).to_queue_config()
+        self.queue_configs[queue_name] = config
+        return self
--- rolecraft/queue_builder.py
+++ rolecraft/queue_builder.py
@@ -1,6 +1,8 @@
+import typing
+
+from .broker import Broker
+from .config import ConfigFetcher, QueueConfig
 from .queue import MessageQueue
-from .config import Config, ConfigFetcher
-from .broker import Broker, default_broker
 
 
 class QueueBuilder:
@@ -9,62 +11,61 @@ class QueueBuilder:
     def __init__(
         self,
         config_fetcher: ConfigFetcher,
-        queue_names: list[str] | None = None,
-        queues: list[MessageQueue] | None = None,
     ):
-        self.queue_names = queue_names
-        self.queues = queues
         self.config_fetcher = config_fetcher
 
-    def build(self) -> list[MessageQueue]:
-        return self._build_queues(self.queue_names, self.queues)
+    def build(
+        self,
+        queue_names: list[str] | None = None,
+        queue_names_per_broker: dict[Broker, list[str]] | None = None,
+        raw_queues: list[MessageQueue] | None = None,
+    ) -> list[MessageQueue]:
+        return self._build_queues(
+            queue_names, queue_names_per_broker, raw_queues
+        )
 
-    def _build_queues(self, queue_names, queues) -> list[MessageQueue]:
+    def _build_queues(
+        self, queue_names, queue_names_per_broker, raw_queues
+    ) -> list[MessageQueue]:
         all_queues = []
 
         if queue_names:
-            all_queues.extend(
-                self._build_queues_with_default_broker(queue_names)
-            )
+            all_queues.extend(self._build_raw_queues(queue_names))
 
-        all_queues.extend(queues)
+        if queue_names_per_broker:
+            for broker, queue_names in queue_names_per_broker.items():
+                all_queues.extend(self._build_raw_queues(queue_names, broker))
+
+        all_queues.extend(raw_queues)
 
         if not all_queues:
             all_queues.append(self._get_default_queue())
 
         return [self._wrap(q) for q in all_queues]
 
-    def _build_queues_with_default_broker(self, queue_names):
-        broker = self._get_default_broker()
+    def _build_raw_queues(self, queue_names, broker: Broker | None = None):
         for queue_name in queue_names:
-            config = self.config_fetcher(queue_name, broker)
-            yield self._new_queue(queue_name, broker, config)
-
-    def _get_default_broker(self) -> Broker:
-        assert default_broker
-        return default_broker
+            config = self.config_fetcher(queue_name, broker=broker)
+            yield self._new_queue(queue_name, config)
 
     def _wrap(self, queue: MessageQueue) -> MessageQueue:
-        config = self.config_fetcher(queue.name, queue.broker)
+        config = self.config_fetcher(queue.name, broker=queue.broker)
         assert config.middlewares
         for middleware in config.middlewares:
-            queue = middleware(queue)
+            queue = typing.cast(MessageQueue, middleware(queue))
             assert isinstance(queue, MessageQueue)
         return queue
 
     def _get_default_queue(self) -> MessageQueue:
-        return self._new_queue(
-            name="default",
-            broker=self._get_default_broker(),
-            config=Config.default(),
-        )
+        default_config = self.config_fetcher()
+        return self._new_queue(name="default", config=default_config)
 
-    def _new_queue(
-        self, name: str, broker: Broker, config: Config
-    ) -> MessageQueue:
+    def _new_queue[M](
+        self, name: str, config: QueueConfig[M]
+    ) -> MessageQueue[M]:
         return MessageQueue(
             name=name,
-            broker=broker,
+            broker=config.broker,
             encoder=config.encoder,
             wait_time_seconds=config.consumer_wait_time_seconds,
         )
