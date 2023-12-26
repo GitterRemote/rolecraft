ed61f84b594c0d4fef1b4a64c0494471b55fa0ec
Sylvan <sylvangreen24@gmail.com>
2023-12-26 21:58:08 +0800

    add ut and extra methods for middleware_list

--- tests/middleware/test_middleware_list.py
+++ tests/middleware/test_middleware_list.py
@@ -1,8 +1,9 @@
-import pytest
+import dataclasses
 from unittest import mock
+
+import pytest
+
 from rolecraft import middleware
-from rolecraft import queue as queue_mod
-from rolecraft import broker as broker_mod
 
 
 @pytest.fixture()
@@ -11,23 +12,48 @@ def empty_middleware_list():
 
 
 @pytest.fixture()
+def retryable():
+    return middleware.Retryable()
+
+
+@pytest.fixture()
 def middleware_list(retryable):
     return middleware.MiddlewareList([retryable])
 
 
-# @pytest.fixture()
-# def broker():
-#     return mock.MagicMock(broker_mod.Broker)
+@dataclasses.dataclass(init=False, eq=True, order=False, repr=False)
+class MiddlewareList(middleware.MiddlewareList):
+    middleware2: mock.MagicMock | None = None
+    middleware3: mock.MagicMock | None = None
 
-
-# @pytest.fixture()
-# def queue(broker):
-#     return queue_mod.Queue(name="default", broker=broker)
+    def _field_name_for(self, middleware) -> str:
+        if getattr(middleware, "_name", None) in (2, 3):
+            return "middleware" + str(middleware._name)
+        return super()._field_name_for(middleware)
 
 
 @pytest.fixture()
-def retryable():
-    return middleware.Retryable()
+def middleware2():
+    middleware = mock.MagicMock()
+    middleware._name = 2
+    return middleware
+
+
+@pytest.fixture()
+def middleware3():
+    middleware = mock.MagicMock()
+    middleware._name = 3
+    return middleware
+
+
+@pytest.fixture()
+def two_middlewares_list(middleware2, middleware3):
+    return MiddlewareList([middleware2, middleware3])
+
+
+@pytest.fixture()
+def three_middlewares_list(retryable, middleware2, middleware3):
+    return MiddlewareList([retryable, middleware2, middleware3])
 
 
 def test_update_non_existing_middleware(empty_middleware_list, retryable):
@@ -56,9 +82,31 @@ def test_modify_a_middleware(middleware_list):
     assert len(middleware_list) == 0
 
 
-def test_overwrite_a_slice(middleware_list):
-    pass
+def test_slice(three_middlewares_list):
+    assert isinstance(three_middlewares_list[0:1], MiddlewareList)
+    middlewares = three_middlewares_list[0:1] + three_middlewares_list[2:]
+    assert len(middlewares) == 2
+    assert isinstance(middlewares, MiddlewareList)
 
 
-def test_concatenate(middleware_list):
-    pass
+class TestAdd:
+    @pytest.fixture(params=["iadd", "add", "radd"])
+    def middlewares_list(self, request, two_middlewares_list, retryable):
+        if request.param == "iadd":
+            two_middlewares_list += [retryable]
+            return two_middlewares_list
+        elif request.param == "add":
+            return two_middlewares_list + [retryable]
+        elif request.param == "radd":
+            return [retryable] + two_middlewares_list
+        else:
+            raise RuntimeError()
+
+    def test_add(self, request, middlewares_list, retryable):
+        assert len(middlewares_list) == 3
+        assert isinstance(middlewares_list, MiddlewareList)
+        assert (
+            middlewares_list[-1] is retryable
+            or middlewares_list[0] is retryable
+        )
+        assert middlewares_list.retryable is retryable
