eb1504a77896a2204d1e184b71c40cbca4a09642
Shuaihu <shuaihu.w@gmail.com>
2023-12-26 12:17:09 +0800

    fix ut

--- tests/config/test_queue_config.py
+++ tests/config/test_queue_config.py
@@ -5,5 +5,10 @@ import pytest
 def test_to_queue_config(incomplete_queue_config, broker, queue_config):
     with pytest.raises(Exception):
         incomplete_queue_config.to_queue_config()
+
+
+def test_to_queue_config_with_added_brorker(
+    incomplete_queue_config, broker, queue_config
+):
     config = dataclasses.replace(incomplete_queue_config, broker=broker)
     assert config.to_queue_config() == queue_config
