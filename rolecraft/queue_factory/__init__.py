51baf3fd2215823ca3f0bc40dfe691dc482605c2
Shuaihu <shuaihu.w@gmail.com>
2023-12-27 12:23:18 +0800

    move QueueAndNameKeys to queue_builder module

--- rolecraft/queue_factory/__init__.py
+++ rolecraft/queue_factory/__init__.py
@@ -1,3 +1,4 @@
-from .queue_factory import QueueFactory, QueueAndNameKeys
+from .queue_builder import QueueAndNameKeys
+from .queue_factory import QueueFactory
 
 __all__ = ["QueueFactory", "QueueAndNameKeys"]
