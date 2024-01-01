8a21bbf3129ed3971e9292f64fa5dbf2788d24a2
Sylvan <sylvangreen24@gmail.com>
2024-01-01 15:37:49 +0800

    remove default_role_hanger from root expose

--- rolecraft/__init__.py
+++ rolecraft/__init__.py
@@ -2,7 +2,7 @@ import rolecraft.thread_local as _thread_local
 from rolecraft.broker import Broker
 from rolecraft.config import ConfigStore
 from rolecraft.config import ConfigurableConfig as Config
-from rolecraft.role_lib import RoleDecorator, default_role_hanger
+from rolecraft.role_lib import RoleDecorator
 from rolecraft.service_factory import ServiceFactory
 
 role = RoleDecorator()
@@ -16,5 +16,4 @@ __all__ = [
     "ConfigStore",
     "Config",
     "local",
-    "default_role_hanger",
 ]
