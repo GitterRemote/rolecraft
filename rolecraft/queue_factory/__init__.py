2b171a491734a3c21c20e6c777ebb06747d39085
Shuaihu <shuaihu.w@gmail.com>
2024-01-13 22:01:57 +0800

    rename QueueAndNameKeys to QueueBuildOptions

--- rolecraft/queue_factory/__init__.py
+++ rolecraft/queue_factory/__init__.py
@@ -1,4 +1,4 @@
-from .queue_builder import QueueAndNameKeys, QueueConfigOptions
+from .queue_builder import QueueBuildOptions, QueueConfigOptions
 from .queue_factory import QueueFactory
 
-__all__ = ["QueueFactory", "QueueAndNameKeys", "QueueConfigOptions"]
+__all__ = ["QueueFactory", "QueueBuildOptions", "QueueConfigOptions"]
