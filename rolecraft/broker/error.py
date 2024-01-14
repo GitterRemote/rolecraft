8d66eed8bad215c72d116885b7b97a2bb906f2ab
Sylvan <sylvangreen24@gmail.com>
2024-01-14 19:03:51 +0800

    add QueueNotFound error

--- rolecraft/broker/error.py
+++ rolecraft/broker/error.py
@@ -12,3 +12,9 @@ class IrrecoverableError(Exception):
 
 class MessageNotFound(IrrecoverableError):
     ...
+
+
+class QueueNotFound(IrrecoverableError):
+    """if auto create queue is disabled for the broker, then it is possible to raise this error."""
+
+    ...
