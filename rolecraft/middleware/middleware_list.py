788464e94e593de09c54190dfdbca67c0c1ba04e
Shuaihu <shuaihu.w@gmail.com>
2024-01-19 20:33:06 +0800

    allow no-name middleware in middlewarelist

--- rolecraft/middleware/middleware_list.py
+++ rolecraft/middleware/middleware_list.py
@@ -1,7 +1,7 @@
 import dataclasses
 import functools
 import typing
-from collections.abc import Iterable, MutableSequence
+from collections.abc import Iterable, MutableSequence, Sequence
 
 from .middleware import Middleware
 from .retryable import Retryable
@@ -16,34 +16,40 @@ class MiddlewareList(MutableSequence[M]):
     _middlewares: list[M]
     retryable: Retryable | None = None
 
-    def __init__(self, middlewares: list[M] | None = None) -> None:
-        middlewares = middlewares or []
-
-        self._set_field("_middlewares", middlewares)
+    def __init__(self, middlewares: Sequence[M] = ()):
+        self._set_field("_middlewares", [])
 
         for middleware in middlewares:
-            self._set_middleware(middleware)
+            self._append_middleware(middleware)
 
-    def _set_middleware(self, middleware: M):
-        field_name = self.field_name_for(middleware)
-        self._set_field(field_name, middleware)
+    def _append_middleware(self, middleware: M):
+        self._middlewares.append(middleware)
+        if field_name := self.name_for(middleware):
+            self._set_field(field_name, middleware)
 
-    def _remove_middleware(self, middleware: M):
-        field_name = self.field_name_for(middleware)
-        self._set_field(field_name, None)
+    def _remove_middleware(
+        self, middleware: M, name: str | None = None, index: int | None = None
+    ):
+        if field_name := name or self.name_for(middleware):
+            self._set_field(field_name, None)
 
-    def field_name_for(self, middleware: M) -> str:
+        if index is not None:
+            popped = self._middlewares.pop(index)
+            assert popped is middleware
+        else:
+            self._middlewares.remove(middleware)
+
+    def name_for(self, middleware: M) -> str | None:
         if isinstance(middleware, Retryable):
             return "retryable"
-        raise TypeError(f"Unknonw middleware type: {middleware}")
+        return None
 
     def _remove_middleware_by_name(self, name: str):
         middleware = getattr(self, name)
         assert middleware, f"middleware doesn't exist for {name}"
-        self._middlewares.remove(middleware)
-        self._set_field(name, None)
+        self._remove_middleware(middleware, name)
 
-    def _set_field(self, name, value):
+    def _set_field(self, name: str, value):
         super().__setattr__(name, value)
 
     @functools.cached_property
@@ -63,32 +69,34 @@ class MiddlewareList(MutableSequence[M]):
             if value is None:
                 self._remove_middleware_by_name(name)
                 return
-            elif self.field_name_for(value) != name:
+            elif self.name_for(value) != name:
                 raise TypeError(f"Middleware type error for {name}: {value}")
 
         super().__setattr__(name, value)
 
     # -- MutableSequence implmenetation --
     def insert(self, index: int, value: M) -> None:
-        field_name = self.field_name_for(value)
-        if getattr(self, field_name):
-            raise ValueError(f"Middleware {field_name} exists")
-        self._set_field(field_name, value)
+        if field_name := self.name_for(value):
+            if getattr(self, field_name):
+                raise ValueError(f"Middleware {field_name} exists")
+            self._set_field(field_name, value)
         return self._middlewares.insert(index, value)
 
     def __setitem__(self, index: int, value: M) -> None:
-        old_field_name = self.field_name_for(self._middlewares[index])
-        field_name = self.field_name_for(value)
-        if old_field_name != field_name:
-            if getattr(self, field_name):
-                raise ValueError(f"Middleware {field_name} exists")
-            self._set_field(old_field_name, None)
-        self._set_field(field_name, value)
+        """Allow to override the middlewares with the same name or add the middleware that isn't in the list."""
+        old_field_name = self.name_for(self._middlewares[index])
+        if field_name := self.name_for(value):
+            if old_field_name != field_name:
+                if getattr(self, field_name):
+                    raise ValueError(f"Middleware {field_name} exists")
+                if old_field_name:
+                    self._set_field(old_field_name, None)
+            self._set_field(field_name, value)
         self._middlewares[index] = value
 
     def __delitem__(self, index: int) -> None:
         middleware = self._middlewares[index]
-        self._remove_middleware(middleware)
+        self._remove_middleware(middleware, index=index)
 
     @typing.overload
     def __getitem__(self, index: int) -> M:
--- tests/middleware/test_middleware_list.py
+++ tests/middleware/test_middleware_list.py
@@ -26,10 +26,10 @@ class MiddlewareList(middleware.MiddlewareList):
     middleware2: mock.MagicMock | None = None
     middleware3: mock.MagicMock | None = None
 
-    def field_name_for(self, middleware) -> str:
+    def name_for(self, middleware) -> str | None:
         if getattr(middleware, "_name", None) in (2, 3):
             return "middleware" + str(middleware._name)
-        return super().field_name_for(middleware)
+        return super().name_for(middleware)
 
 
 @pytest.fixture()
@@ -43,6 +43,13 @@ def middleware2():
 def middleware3():
     middleware = mock.MagicMock()
     middleware._name = 3
+    return middleware
+
+
+@pytest.fixture()
+def middleware_no_name():
+    middleware = mock.MagicMock()
+    middleware._name = 4
     return middleware
 
 
@@ -80,6 +87,44 @@ def test_iter(middleware_list):
 def test_modify_a_middleware(middleware_list):
     middleware_list.retryable = None
     assert len(middleware_list) == 0
+
+
+def test_del(middleware_list):
+    del middleware_list[0]
+    assert len(middleware_list) == 0
+    assert middleware_list.retryable is None
+
+
+def test_set_with_index(retryable, middleware2, middleware3):
+    middlewares = MiddlewareList([retryable, middleware2])
+    assert middlewares.retryable
+    assert middlewares.middleware2
+
+    middlewares[1] = middleware3
+    assert not middlewares.middleware2
+    assert middlewares.middleware3 is middleware3
+
+    with pytest.raises(ValueError):
+        middlewares[1] = retryable
+
+
+def test_middleware_without_name(
+    middleware_no_name, retryable, middleware2, middleware3
+):
+    middleware_list = [retryable, middleware_no_name, middleware2]
+    middlewares = MiddlewareList(middleware_list)
+    assert len(middlewares) == 3
+    assert middlewares.retryable
+    assert middlewares.middleware2
+    assert list(middlewares) == middleware_list
+
+    middlewares.append(middleware_no_name)
+    assert len(middlewares) == 4
+
+    middlewares[1] = middleware3
+    assert len(middlewares) == 4
+    assert middlewares[1] is middleware3
+    assert middlewares.middleware3 is middleware3
 
 
 def test_slice(three_middlewares_list):
