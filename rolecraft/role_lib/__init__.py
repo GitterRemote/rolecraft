7d808d3a20000419c2ce010f1b61380a153d8e8c
Shuaihu <shuaihu.w@gmail.com>
2024-01-21 18:49:51 +0800

    expose role error

--- rolecraft/role_lib/__init__.py
+++ rolecraft/role_lib/__init__.py
@@ -1,5 +1,26 @@
+from .error import (
+    ActionError,
+    CraftError,
+    DeserializeError,
+    DispatchError,
+    RoleError,
+    SerializeError,
+    UnmatchedQueueNameError,
+)
 from .role import Role
 from .role_decorator import RoleDecorator
 from .role_hanger import RoleHanger, default_role_hanger
 
-__all__ = ["Role", "RoleDecorator", "RoleHanger", "default_role_hanger"]
+__all__ = [
+    "Role",
+    "RoleDecorator",
+    "RoleHanger",
+    "default_role_hanger",
+    "ActionError",
+    "CraftError",
+    "DeserializeError",
+    "SerializeError",
+    "UnmatchedQueueNameError",
+    "DispatchError",
+    "RoleError",
+]
