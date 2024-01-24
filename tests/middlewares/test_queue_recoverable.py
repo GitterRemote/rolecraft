ce2dc9da8a6e92d20edddeaa49fa89f0b6214fcc
Shuaihu <shuaihu.w@gmail.com>
2024-01-24 20:17:27 +0800

    add ut for QueueRecoverable

--- tests/middlewares/test_queue_recoverable.py
+++ tests/middlewares/test_queue_recoverable.py
@@ -1,6 +1,8 @@
+from unittest import mock
+
 import pytest
 
-from rolecraft.broker import RecoverableError
+from rolecraft.broker import IrrecoverableError, RecoverableError
 from rolecraft.message import Message
 from rolecraft.middlewares import queue_recoverable as queue_recoverable_mod
 
@@ -30,13 +32,39 @@ def test_recoverable_error_for_receive(queue, middleware, new_message):
     assert queue.receive.call_count == 2
 
 
-def test_recoverable_error_for_ack():
-    ...
+def test_recoverable_error_for_ack(queue, middleware, new_message):
+    queue.ack.__name__ = "ack"
+    queue.ack.side_effect = [RecoverableError, None]
+    assert callable(queue.ack)
+
+    msg = new_message()
+    rv = middleware.ack(msg)
+    assert rv is None
+    assert queue.ack.call_count == 2
+    queue.ack.assert_has_calls([mock.call(msg), mock.call(msg)])
 
 
-def test_irrecoverable_error():
-    ...
+def test_irrecoverable_error(queue, middleware, new_message):
+    queue.receive.side_effect = [IrrecoverableError, [new_message()]]
+    assert callable(queue.receive)
+
+    with pytest.raises(IrrecoverableError):
+        middleware.receive()
+
+    assert queue.receive.call_count == 1
 
 
-def test_recoverable_error_exceeds_max_retries():
-    ...
+def test_recoverable_error_exceeds_max_retries(queue, middleware, new_message):
+    middleware.queue_retries = 2
+    msgs = [new_message(), new_message()]
+    queue.receive.side_effect = [
+        RecoverableError,
+        RecoverableError,
+        RecoverableError,
+        msgs,
+    ]
+    assert callable(queue.receive)
+
+    with pytest.raises(RecoverableError):
+        middleware.receive()
+    assert queue.receive.call_count == 3
