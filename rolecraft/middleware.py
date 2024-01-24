378424cdbe06b93ed7ad6d1691b7368378dfbd0f
Sylvan <sylvangreen24@gmail.com>
2024-01-24 10:17:15 +0800

    add BaseMiddleware

--- rolecraft/middleware.py
+++ rolecraft/middleware.py
@@ -1,61 +1,22 @@
+import abc
 from typing import Self
 
-from rolecraft.message import Message
 from rolecraft.queue import MessageQueue
 
 
-class UninitiatedError(Exception):
+class MiddlewareError(Exception):
     ...
 
 
 @MessageQueue.register
-class Middleware:
+class Middleware(abc.ABC):
     def __init__(self, queue: MessageQueue | None = None) -> None:
         self.queue: MessageQueue | None = queue
-        self.is_outmost = False
 
     def __getattr__(self, name):
         return getattr(self.queue, name)
 
+    @abc.abstractmethod
     def __call__(self, queue: MessageQueue) -> Self:
-        wrapped = self.copy_with(queue)
-
-        # Update is_outmost attribute
-        if getattr(queue, "is_outmost", False) is True:
-            queue.is_outmost = False  # type: ignore
-        wrapped.is_outmost = True
-
-        return wrapped
-
-    def copy_with(self, queue: MessageQueue) -> Self:
-        return self.__class__(queue=queue, **self.options)
-
-    # -- Update message's queue attribute --
-
-    def _update_message(self, message: Message) -> Message:
-        message.queue = self  # type: ignore
-        return message
-
-    def block_receive(self, *args, **kwargs):
-        """If the wait_time_seconds is None, it will be default value of the
-        queue."""
-        if not self.queue:
-            raise UninitiatedError
-
-        future = self.queue.block_receive(*args, **kwargs)
-        return (
-            future.transform(
-                lambda msgs: list(map(self._update_message, msgs))
-            )
-            if self.is_outmost
-            else future
-        )
-
-    def receive(self, *args, **kwargs):
-        if not self.queue:
-            raise UninitiatedError
-
-        msgs = self.queue.receive(*args, **kwargs)
-        if self.is_outmost:
-            return [self._update_message(m) for m in msgs]
-        return msgs
+        """Create a new middleware instance with the current options and the passed-in queue"""
+        ...
